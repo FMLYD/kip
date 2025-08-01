4c42fd765ff9952925ff618504255e680d8a99c8
FMLYD <102463605+FMLYD@users.noreply.github.com>
2025-08-02 02:49:01 +0800

    Update bt.sh

--- scripts/bt.sh
+++ scripts/bt.sh
@@ -8,9 +8,9 @@ python  benchmark_spi.py \
   --outpath './results' \
   --lr 0.01 \
   --seed 2024 \
-  --epochs 50 \
+  --epochs 500 \
   --batch_size 128 \
   --p $p\
-  --stop 20 \
-  --sigma "0.1,0.3,0.7,1,3,5,7" 
+  --stop 10 \
+  --sigma "0.1,0.3,0.5,0.7,1,3,5" 
 done
