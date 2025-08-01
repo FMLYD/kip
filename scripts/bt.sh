c19ec75492e2c9f944ca649c13f06457b13e8a13
FMLYD <102463605+FMLYD@users.noreply.github.com>
2025-08-01 22:51:22 +0800

    Add files via upload

---
+++ scripts/bt.sh
@@ -0,0 +1,16 @@
+export CUDA_VISIBLE_DEVICES=2
+
+for p in  0.1 0.2 0.3 0.4
+do
+python  benchmark_spi.py \
+  --model KPI \
+  --dataset_name  blood_transfusion  \
+  --outpath './results' \
+  --lr 0.01 \
+  --seed 2024 \
+  --epochs 50 \
+  --batch_size 128 \
+  --p $p\
+  --stop 20 \
+  --sigma "0.1,0.3,0.5,0.7,0.9,1,3,5,7" 
+done
---
+++ scripts/qb.sh
@@ -0,0 +1,16 @@
+export CUDA_VISIBLE_DEVICES=2
+
+for p in 0.1 
+do
+python  exper_standard_kip.py \
+  --model KPI \
+  --dataset_name qsar_biodegradation \
+  --outpath './results' \
+  --lr 0.01 \
+  --seed 2024 \
+  --epochs 50 \
+  --batch_size 128 \
+  --p $p \
+  --sigma "0.1,0.3,0.5,0.7,0.9,1,3,5,7" 
+done
+
