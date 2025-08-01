49373c2073c846f14fbbc63c810c29c9c3c686e7
FMLYD <102463605+FMLYD@users.noreply.github.com>
2025-08-02 02:48:18 +0800

    Update qb.sh

--- scripts/qb.sh
+++ scripts/qb.sh
@@ -8,9 +8,9 @@ python  exper_standard_kip.py \
   --outpath './results' \
   --lr 0.01 \
   --seed 2024 \
-  --epochs 50 \
+  --epochs 500 \
   --batch_size 128 \
   --p $p \
-  --sigma "0.1,0.3,0.5,0.7,0.9,1,3,5,7" 
+  --sigma "0.1,0.3,0.5,0.7,0.9,1,3" 
 done
 
