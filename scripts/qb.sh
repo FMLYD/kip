eb457b0d1de6f9e39e0739dfb30ccd1f168e6e1d
FMLYD <102463605+FMLYD@users.noreply.github.com>
2025-08-06 19:28:32 +0800

    Update qb.sh

--- scripts/qb.sh
+++ scripts/qb.sh
@@ -1,16 +1,57 @@
 export CUDA_VISIBLE_DEVICES=2
-
-for p in 0.1 
-do
-python  exper_standard_kip.py \
+python  benchmark_kpi.py \
   --model KPI \
   --dataset_name qsar_biodegradation \
   --outpath './results' \
   --lr 0.01 \
   --seed 2024 \
-  --epochs 500 \
+  --epochs 50 \
   --batch_size 128 \
   --p $p \
-  --sigma "0.1,0.3,0.5,0.7,1,3,5" 
-done
+  --sigma "0.1,0.3,0.5,0.7,1,3,5" \
+  --k 2\
+  --metric nan_manhattan \
+  --weights distance
+  
+  python  benchmark_kpi.py \
+  --model KPI \
+  --dataset_name qsar_biodegradation \
+  --outpath './results' \
+  --lr 0.01 \
+  --seed 2024 \
+  --epochs 50 \
+  --batch_size 128 \
+  --p $p \
+  --sigma "0.1,0.3,0.5,0.7,1,3,5" 、
+  --k 3\
+  --metric nan_manhattan \
+  --weights distance
 
+  
+  python  benchmark_kpi.py \
+  --model KPI \
+  --dataset_name qsar_biodegradation \
+  --outpath './results' \
+  --lr 0.01 \
+  --seed 2024 \
+  --epochs 50 \
+  --batch_size 128 \
+  --p $p \
+  --sigma "0.1,0.3,0.5,0.7,1,3,5" \
+  --k 3\
+  --metric nan_manhattan \
+  --weights distance
+  
+  python  benchmark_kpi.py \
+  --model KPI \
+  --dataset_name qsar_biodegradation \
+  --outpath './results' \
+  --lr 0.01 \
+  --seed 2024 \
+  --epochs 50 \
+  --batch_size 128 \
+  --p 5 \
+  --sigma "0.1,0.3,0.5,0.7,1,3,5" \
+  --k 5\
+  --metric nan_manhattan \
+  --weights distance
