f66c47adf465cbf071d8f5d4fc606945d70c783f
FMLYD <102463605+FMLYD@users.noreply.github.com>
2025-08-06 19:24:26 +0800

    Update cc.sh

--- scripts/cc.sh
+++ scripts/cc.sh
@@ -1,7 +1,5 @@
 export CUDA_VISIBLE_DEVICES=2
 
-for p in 0.1 0.2 0.3 0.4
-do
 python  exper_standard_kip.py \
   --model KPI \
   --dataset_name concrete_compression \
@@ -10,7 +8,55 @@ python  exper_standard_kip.py \
   --seed 2024 \
   --epochs 500 \
   --batch_size 128 \
-  --p $p \
+  --p 0.1 \
   --stop 20 \
-  --sigma "0.1,0.3,0.5,0.7,0.9,1,3,5,7" 
-done
+  --sigma "0.1,0.3,0.5,0.7,0.9,1,3,5,7"\
+  --k 4\
+  --metric nan_manhattan \
+  --weights distance
+
+  python  exper_standard_kip.py \
+  --model KPI \
+  --dataset_name concrete_compression \
+  --outpath './results' \
+  --lr 0.01\
+  --seed 2024 \
+  --epochs 500 \
+  --batch_size 128 \
+  --p 0.2 \
+  --stop 20 \
+  --sigma "0.1,0.3,0.5,0.7,0.9,1,3,5,7" \
+  --k 3\
+  --metric nan_manhattan \
+  --weights distance
+
+  python  exper_standard_kip.py \
+  --model KPI \
+  --dataset_name concrete_compression \
+  --outpath './results' \
+  --lr 0.01\
+  --seed 2024 \
+  --epochs 500 \
+  --batch_size 128 \
+  --p 0.3 \
+  --stop 20 \
+  --sigma "0.1,0.3,0.5,0.7,0.9,1,3,5,7" \
+  --k 30\
+  --metric nan_manhattan \
+  --weights distance
+
+  python  exper_standard_kip.py \
+  --model KPI \
+  --dataset_name concrete_compression \
+  --outpath './results' \
+  --lr 0.01\
+  --seed 2024 \
+  --epochs 500 \
+  --batch_size 128 \
+  --p 0.4 \
+  --stop 20 \
+  --sigma "0.1,0.3,0.5,0.7,0.9,1,3,5,7"\
+  --k 90\
+  --metric nan_manhattan \
+  --weights distance
+  
